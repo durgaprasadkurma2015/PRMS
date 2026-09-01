1f9f93f8c372e06470cdeeff7217869bcff3a15c
Kurma Durga Prasad <durgaprasadkurma2015@gmail.com>
2026-09-01 10:07:14 +0530

    Initial PRMS Project

---
+++ backend/prms/src/test/java/com/example/prms/PrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
